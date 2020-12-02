11194245443a638a80ed6f7d09ec0ea6c21d582b
A <hallo@alexanderboenninger.de>
2020-12-02 16:45:40 +0100

    Update error.php

--- site/templates/error.php
+++ site/templates/error.php
@@ -1,2 +1,2 @@
 <?php snippet('header') ?>
-<h2>Sorry, this site dosn't exist. visit me here: <a href="index.html">www.sterling-simpsion-johnson.com</a> </h2>
+<h2>Sorry, this site dosn't exist. visit me here: <a href="index.html">www.walz.design</a> </h2>
