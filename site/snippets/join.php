a2c8d3576171a9e0fc665d56d9caafc64b705a64
A <hallo@alexanderboenninger.de>
2021-02-17 00:57:01 +0100

    Mail links

--- site/snippets/join.php
+++ site/snippets/join.php
@@ -2,7 +2,7 @@
       <div class="container join_content content space_top_80p space_bottom_80p">
         <p class="smal_brot join_walzer"> <?= $join->walzer()->ktr() ?></p>
 
-        <a class="button click_ani join_walzer_button" href="mailto:hello@designwalz.com&amp;subject=Join%20as%20Walzer&amp;body=Hallo.%0D%0AI%20am%20interessted%20to%20join%20the%20Design%20Walz%20as%20Walzer.">
+        <a class="button click_ani join_walzer_button" href="mailto:hello@designwalz.com?subject=Join%20as%20Walzer&amp;body=Hallo.%0D%0AI%20am%20interessted%20to%20join%20the%20Design%20Walz%20as%20Walzer.">
           <div class="stack stack_1_layer yellow">
             <p class="big_brot button_text"> <?= $join->walzer_button()->ktr() ?></p>
           </div>
@@ -10,7 +10,7 @@
           </div>
         </a>
         <p class="smal_brot join_host"> <?= $join->host()->ktr() ?></p>
-        <a class="button click_ani join_host_button" href="mailto:hello@designwalz.com&amp;subject=Join%20as%20Host&amp;body=Hallo.%0D%0AWe%20are%20interessted%20in%20hosting%20a%20Walzer." >
+        <a class="button click_ani join_host_button" href="mailto:hello@designwalz.com?subject=Join%20as%20Host&amp;body=Hello.%0D%0AWe%20are%20interessted%20in%20hosting%20a%20Walzer.">
           <div class="stack stack_1_layer yellow">
             <p class="big_brot button_text"> <?= $join->host_button()->ktr() ?></p>
           </div>
--- site/snippets/support-banner.php
+++ site/snippets/support-banner.php
@@ -2,7 +2,7 @@
   <div class="container support_content content space_top_80p space_bottom_80p">
     <p class="smal_brot join_walzer white_typo"> <?= $support->support_text()->ktr() ?></p>
 
-    <a class="button click_ani join_walzer_button white_typo" href="mailto:hello@walz.design&amp;subject=Support%20the%20Design%20Walz&amp;body=Hello.%0D%0AI%20would%20love%20to%20support%20you." >
+    <a class="button click_ani join_walzer_button white_typo" href="mailto:hello@designwalz.com?subject=Support%20the%20Design%20Walz&amp;body=Hello.%0D%0AI%20would%20love%20to%20support%20you.">
       <div class="stack stack_1_layer white_border green">
         <p class="big_brot button_text"> <?= $support->support_button()->ktr() ?></p>
       </div>
@@ -11,7 +11,7 @@
     </a>
 
     <p class="smal_brot join_host white_typo"> <?= $support->donate_text()->ktr() ?></p>
-    <a class="button click_ani join_host_button white_typo" href="mailto:hello@walz.design&amp;subject=Donation&amp;body=Hallo.%0D%0AWe%20are%20still%20figuring%20out%20how%20we%20can%20proceed%20Donations.%20If%20you%20contact%20us,%20we%20will%20find%20a%20quick%20solution." >
+    <a class="button click_ani join_host_button white_typo" href="mailto:hello@designwalz.com?subject=Donation&amp;body=Hallo.%0D%0AWe%20are%20still%20figuring%20out%20how%20we%20can%20proceed%20Donations.%20If%20you%20contact%20us,%20we%20will%20find%20a%20quick%20solution.">
       <div class="stack stack_1_layer white_border green">
         <p class="big_brot button_text"> <?= $support->donate_button()->ktr() ?></p>
       </div>
